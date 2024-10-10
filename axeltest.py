b43c1ce799fa08daba2cdf776bc71e808b67dcdc
Axel-Svendsen <axel.0313axel@gmail.com>
2024-10-10 22:25:20 +0200

    addade min testfil till gitignore

--- axeltest.py
+++ axeltest.py
@@ -1,3 +1,11 @@
 s = "1.4                            4.5\n"
 
 print([float(j) for j in s.split()])
+
+
+dict = {namn: user(djkdfl)
+ annatnamn: user}
+
+insant = user()
+
+dict["axel"].spendmessage("linus")
